e6cc51539edfde4c008f385a9153fca675886b03
pedroivodev <pedroivofe@gmail.com>
2018-07-22 17:40:55 -0300

    Diminuindo a resposabilidade do método de login

--- backend/api/seguranca/base/UsuariosDao.js
+++ backend/api/seguranca/base/UsuariosDao.js
@@ -11,15 +11,9 @@ function login({email, senha, next, nextErroBase }) {
     `select usu.nome nome_usuario, usu.sobrenome sobrenome_usuario,
     usu.email, usu.senha,
     grupos.id id_grupo,
-    grupos.nome nome_grupo,
-    telas.id id_tela,
-    telas.nome nome_tela,
-    telas.fk_id_tela,
-    telas.path
+    grupos.nome nome_grupo
     from usuarios usu
-    join acessos on acessos.fk_id_grupo = usu.fk_id_grupo
-    join grupos on grupos.id = acessos.fk_id_grupo
-    join telas on telas.id = acessos.fk_id_tela
+    join grupos on grupos.id = usu.fk_id_grupo
     where email = ? AND ativo = true`, [email],
     function (err, results) {
         if (err) {
@@ -39,41 +33,10 @@ function login({email, senha, next, nextErroBase }) {
                     email: results[0].email,
                     nome: results[0].nome_usuario,
                     grupo: {
+                        id: results[0].id_grupo,
                         nome: results[0].nome_grupo,
-                    },
-                    acessos: []
-                }
-
-                let telasPermitidas = []
-                for (let i = 0; i < results.length; i++) {
-                    telasPermitidas.push({
-                        id: results[i].id_tela,
-                        nome: results[i].nome_tela,
-                        fk_id_tela: results[i].fk_id_tela,
-                        path: results[i].path,
-                    })
-                }
-
-                let idTelasAchadas = []
-
-                let telasPermitidasOrdenadas = telasPermitidas.filter((tela, indice) => {
-                    if (!tela.fk_id_tela) {
-                        idTelasAchadas.push(tela.id)
-                        return true
                     }
-
-                    return false
-                })
-
-                let telasRestantes = getListaAtualizada(telasPermitidas, idTelasAchadas)
-                while (telasRestantes.length != 0) {
-                    searchInTelas(telasPermitidasOrdenadas, telasRestantes[0])
-
-                    idTelasAchadas.push(telasRestantes[0].id)
-                    telasRestantes = getListaAtualizada(telasRestantes, idTelasAchadas)
                 }
-
-                informacoesUsuario.telasPermitidas = telasPermitidasOrdenadas
 
                 next({informacoesUsuario})
             } else {
