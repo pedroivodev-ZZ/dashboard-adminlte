d3381d71a17289c233458a7a15e64c63fe8af0f6
pedroivodev <pedroivofe@gmail.com>
2018-07-22 19:43:47 -0300

    Transformando componente Box em "função pura"

--- frontend/src/componentes/base_layout/Box.jsx
+++ frontend/src/componentes/base_layout/Box.jsx
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class Box extends Component {
-    render() {
-        const { novoButton, novoButtonText, novoButtonClick } = this.props
-        return (
-            <div className="box">
-                <div className="box-header">
-                    <h3 className="box-title">{this.props.titulo}</h3>
-                    {
-                        (novoButton === undefined || novoButton === true) ?
-                        <button className="btn btn-info btn-flat pull-right"
-                        onClick={novoButtonClick}>
-                            {novoButtonText}
-                        </button>
-                        : null
-                    }
-                </div>
-                <div className="box-body">
-                    {this.props.children}
-                </div>
-            </div>
-        )
-    }
-}
+const Box = ({ titulo, novoButton, novoButtonText, novoButtonClick, children }) => (
+    <div className="box">
+        <div className="box-header">
+            <h3 className="box-title">{titulo}</h3>
+            {
+                (novoButton === undefined || novoButton === true) ?
+                <button className="btn btn-info btn-flat pull-right"
+                onClick={novoButtonClick}>
+                    {novoButtonText}
+                </button>
+                : null
+            }
+        </div>
+        <div className="box-body">
+            {children}
+        </div>
+    </div>
+)
 
 Box.propTypes = {
     novoButtonClick: PropTypes.func,
     novoButtonText: PropTypes.string,
-    novoButton: PropTypes.bool
+    novoButton: PropTypes.bool,
+    children: PropTypes.node
 }
 
 export default Box
